6d3368d8d155ef3e0d3d978963ee7bc096cf8b7c
ChildishCD <51065018+ChildishCD@users.noreply.github.com>
2023-12-18 21:45:21 +0800

    array

--- 1-SqList.cpp
+++ 1-SqList.cpp
@@ -14,6 +14,8 @@ q = (int*)malloc(sizeof(int) * n);  // 申请n+1个位置的辅助空间
 for (int i = 0; i < n; i++)         // 数组元素初值置0
     *(q + i) = 0;                   // 获取数组值的方法
 free(q);                            // 回收存储空间
+// 3 c++
+a = new int[n]
 
 // 线性表——顺序存储
 // SqList
